4e9b1a7cf91eb74e69c65779534b831f580318b4
JNSreeUma <jakkulauma33@gmail.com>
2026-04-26 16:00:26 +0530

    fixed port issue

--- templates/node-app/index.ts
+++ templates/node-app/index.ts
@@ -6,8 +6,8 @@ app.get('/', (req, res) => {
   res.send("Hello from Atlas TS Day 21 Automation test 9🚀");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT || "3000", 10);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`App running on port ${PORT}`);
 });
