b22776484258e559e93d93fc61a3a43d0f198f09
JNSreeUma <jakkulauma33@gmail.com>
2026-04-26 21:19:01 +0530

    fixed test message

--- templates/node-app/index.ts
+++ templates/node-app/index.ts
@@ -12,6 +12,6 @@ app.get("/health", (req, res) => {
   res.status(200).send("OK");
 });
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`App running on port ${PORT}`);
-});
+app.listen(PORT, "0.0.0.0");
+  // console.log(`App running on port ${PORT}`);
+// });
