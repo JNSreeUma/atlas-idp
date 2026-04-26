dc2b6281534732ac741d421275cf05abf32efd0b
JNSreeUma <jakkulauma33@gmail.com>
2026-04-26 16:10:31 +0530

    fixed issues

--- templates/node-app/index.ts
+++ templates/node-app/index.ts
@@ -2,11 +2,15 @@ import express from 'express';
 
 const app = express();
 
+const PORT = Number(process.env.PORT) || 3000;
+
 app.get('/', (req, res) => {
-  res.send("Hello from Atlas TS Day 21 Automation test 9🚀");
+  res.send("Hello from Atlas TS Day 21 Automation test 12🚀");
 });
 
-const PORT = parseInt(process.env.PORT || "3000", 10);
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`App running on port ${PORT}`);
