5f82e8c8653ff487d2d750caeb2be73d420e0b2d
JNSreeUma <jakkulauma33@gmail.com>
2026-04-26 22:58:05 +0530

    Automated Secrets

--- templates/node-app/index.ts
+++ templates/node-app/index.ts
@@ -5,13 +5,13 @@ const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
 app.get('/', (req, res) => {
-  res.send("Hello from Atlas TS Day 22 Automation test Final Happy ending love you... 🚀");
+  res.send("Hello from Atlas TS Day 22 Automation test Final Happy ending love you ... 🚀");
 });
 
 app.get("/health", (req, res) => {
   res.status(200).send("OK");
 });
 
-app.listen(PORT, "0.0.0.0");
-  // console.log(`App running on port ${PORT}`);
-// });
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`App running on port ${PORT}`);
+});
