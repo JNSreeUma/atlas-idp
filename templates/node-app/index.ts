879fa87e37d0261c3867db15dc7b274e6c0114c4
JNSreeUma <jakkulauma33@gmail.com>
2026-04-26 15:42:59 +0530

    fixed ecs errors testing

--- templates/node-app/index.ts
+++ templates/node-app/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("Hello from Atlas TS Day 21 Automation🚀");
+  res.send("Hello from Atlas TS Day 21 Automation test 7🚀");
 });
 
 const PORT = process.env.PORT || 3000;
